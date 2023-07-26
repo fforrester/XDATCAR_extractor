1ee7f9321e650974a6ff65c509fab3c8fc076a7f
Frazer Forrester <75732212+fforrester@users.noreply.github.com>
2023-07-26 18:53:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,8 @@
-"""
-Utilities for VASP AIMD analysis.
-"""
-
 from setuptools import setup, find_packages
 from AIMD_extractor.version import __version__ as VERSION
 
 readme = "README.md"
 long_description = open(readme).read()
-
-# Get the path of the current directory
-current_directory = os.path.abspath(os.path.dirname(__file__))
-
-# Include the configuration files in the package data
-package_data = {
-    'AIMD_extractor': ['temperature_variations.json', 'run_variations.json'],
-}
 
 setup(
     name="AIMD_extractor",
@@ -25,17 +13,18 @@ setup(
     author="Frazer N. Forrester",
     author_email="f.forrester2@ncl.ac.uk",
     url="https://github.com/fforrester/AIMD_extractor",
-    download_url="https://github.com/fforrester/AIMD_extractor/archive/{}.tar.gz".format(
-        VERSION
-    ),
+    download_url="https://github.com/fforrester/AIMD_extractor/archive/{}.tar.gz".format(VERSION),
     license="MIT",
-    install_requires=["pymatgen","pymatgen-analysis-diffusion","numpy"],
+    packages=find_packages(),
+    package_data={
+        'AIMD_extractor': ['temperature_variations.json', 'run_variations.json']
+    },
+    install_requires=["pymatgen", "pymatgen-analysis-diffusion", "numpy"],
     python_requires=">=3.7",
     entry_points={
         'console_scripts': [
             'AIMD_extractor = AIMD_extractor.AIMD_extractor:main',
         ],
     },
-    package_data=package_data,
-    include_package_data=True,
 )
+
