cf0a17a9931b717a1ba9143a6739fdff2f2950fd
Frazer Forrester <frazerforrester@outlook.com>
2024-02-22 10:33:47 +0000

    name change

--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
 from setuptools import setup, find_packages
-from AIMD_extractor.version import __version__ as VERSION
+from XDATCAR_extractor.version import __version__ as VERSION
 
 with open("README.md","r") as f:
     long_description = f.read()
 
 setup(
-    name="AIMD_extractor",
+    name="XDATCAR_extractor",
     version=VERSION,
-    description="Python utilities for VASP AIMD analysis",
+    description="Python utilities for XDATCAR analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Frazer N. Forrester",
-    author_email="f.forrester2@ncl.ac.uk",
-    url="https://github.com/fforrester/AIMD_extractor",
-    download_url="https://github.com/fforrester/AIMD_extractor/archive/{}.tar.gz".format(VERSION),
+    author_email="frazer.forrester@ncl.ac.uk",
+    url="https://github.com/fforrester/XDATCAR_extractor",
+    download_url="https://github.com/fforrester/XDATCAR_extractor/archive/{}.tar.gz".format(VERSION),
     license="MIT",
     packages=find_packages(),
     package_data={
-        'AIMD_extractor': ['temperature_variations.json', 'run_variations.json']
+        'XDATCAR_extractor': ['temperature_variations.json', 'run_variations.json']
     },
     install_requires=["pymatgen", "pymatgen-analysis-diffusion", "numpy"],
     python_requires=">=3.7",
     entry_points={
         'console_scripts': [
-            'AIMD_extractor = AIMD_extractor.AIMD_extractor:main',
+            'XDATCAR_extractor = XDATCAR_extractor.XDATCAR_extractor:main',
         ],
     },
 )
